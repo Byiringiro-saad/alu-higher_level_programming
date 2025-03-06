9c73ae2f90d2b4281ac50f077f5f03b71d560b0d
Byiringiro-saad <byiringirosaad@gmail.com>
2025-03-06 06:50:25 -0800

    five

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,6 +2,6 @@
 def print_alphabet():
     for i in range(25, -1, -1):
         if i % 2 == 0:
-            print(chr(122 - i), end="")
+            print("{}".format(chr(122 - i)), end="")
         else:
-            print(chr(122 - i).upper(), end="")
+            print("{}".format(chr(122 - i).upper()), end="")
