f8346018d38d25aa91f75a52386bfa9ba8cefbb8
Byiringiro-saad <byiringirosaad@gmail.com>
2025-03-14 10:54:43 -0700

    sql

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,4 +1,8 @@
 -- This script lists all databases on the MySQL server
 
-SELECT SCHEMA_NAME
-FROM INFORMATION_SCHEMA.SCHEMATA;
+SELECT DISTINCT SCHEMA_NAME
+FROM INFORMATION_SCHEMA.SCHEMATA
+WHERE SCHEMA_NAME != 'information_schema'
+  AND SCHEMA_NAME != 'performance_schema'
+  AND SCHEMA_NAME != 'mysql'
+  AND SCHEMA_NAME != 'sys';
