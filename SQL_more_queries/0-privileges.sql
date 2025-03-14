b75e0b474971991e0f9ecd9e9043a615afd8e35a
Byiringiro-saad <byiringirosaad@gmail.com>
2025-03-14 12:55:14 -0700

    another

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,7 @@
 -- Task: List all privileges of MySQL users 'user_0d_1' and 'user_0d_2' on localhost
 
 -- Show privileges for user 'user_0d_1' on localhost
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
 -- Show privileges for user 'user_0d_2' on localhost
-DROP USER IF EXISTS 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
