f452789876e304df9a571a0e0a30b5f9d726ee59
Byiringiro-saad <byiringirosaad@gmail.com>
2025-03-14 11:13:49 -0700

    sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,20 +1,20 @@
--- This script generates the CREATE TABLE syntax for the table 'first_table'
+-- This script generates the CREATE TABLE statement for the 'first_table'
 
 SELECT 
     CONCAT(
-        'CREATE TABLE `', TABLE_NAME, '` (', GROUP_CONCAT(
+        TABLE_NAME, ' CREATE TABLE `', TABLE_NAME, '` (',
+        GROUP_CONCAT(
             CONCAT(
                 '`', COLUMN_NAME, '` ', COLUMN_TYPE,
                 IF(IS_NULLABLE = 'NO', ' NOT NULL', ''),
                 IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT ', COLUMN_DEFAULT), ''),
                 IF(EXTRA != '', CONCAT(' ', EXTRA), '')
-            ) 
-            ORDER BY ORDINAL_POSITION SEPARATOR ', '
-        ), ', PRIMARY KEY(`id`)', ') ENGINE=', ENGINE, 
+            ) ORDER BY ORDINAL_POSITION SEPARATOR ', '), 
+        ', PRIMARY KEY(`id`)) ENGINE=', ENGINE,
         ' DEFAULT CHARSET=', TABLE_COLLATION
     ) AS create_table_query
 FROM INFORMATION_SCHEMA.COLUMNS
-JOIN INFORMATION_SCHEMA.TABLES 
+JOIN INFORMATION_SCHEMA.TABLES
     ON COLUMNS.TABLE_NAME = TABLES.TABLE_NAME 
     AND COLUMNS.TABLE_SCHEMA = TABLES.TABLE_SCHEMA
 WHERE COLUMNS.TABLE_SCHEMA = DATABASE() 
