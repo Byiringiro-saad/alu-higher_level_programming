4c45a10afa03c94ce845f411847d530a11b7f772
Byiringiro-saad <byiringirosaad@gmail.com>
2025-03-14 11:10:53 -0700

    sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
--- This script prints the full description of the table first_table
+-- This script prints the full description of the table first_table from the given database
 
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'first_table';
