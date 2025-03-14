f23f34784247124e12bffc5918b4cc7a558b9cfb
Byiringiro-saad <byiringirosaad@gmail.com>
2025-03-14 11:15:07 -0700

    sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,22 +1,10 @@
--- This script generates the CREATE TABLE statement for the 'first_table'
+-- This script prints the full description of the table 'first_table' from the given database
 
 SELECT 
-    CONCAT(
-        TABLE_NAME, ' CREATE TABLE `', TABLE_NAME, '` (',
-        GROUP_CONCAT(
-            CONCAT(
-                '`', COLUMN_NAME, '` ', COLUMN_TYPE,
-                IF(IS_NULLABLE = 'NO', ' NOT NULL', ''),
-                IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT ', COLUMN_DEFAULT), ''),
-                IF(EXTRA != '', CONCAT(' ', EXTRA), '')
-            ) ORDER BY ORDINAL_POSITION SEPARATOR ', '), 
-        ', PRIMARY KEY(`id`)) ENGINE=', ENGINE,
-        ' DEFAULT CHARSET=', TABLE_COLLATION
-    ) AS create_table_query
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_DEFAULT, 
+    EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-JOIN INFORMATION_SCHEMA.TABLES
-    ON COLUMNS.TABLE_NAME = TABLES.TABLE_NAME 
-    AND COLUMNS.TABLE_SCHEMA = TABLES.TABLE_SCHEMA
-WHERE COLUMNS.TABLE_SCHEMA = DATABASE() 
-  AND COLUMNS.TABLE_NAME = 'first_table'
-GROUP BY COLUMNS.TABLE_NAME, ENGINE, TABLE_COLLATION;
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'first_table';
