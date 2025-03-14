4124630622a912bbb4f0e7dea578fbdc9a976ba8
Byiringiro-saad <byiringirosaad@gmail.com>
2025-03-14 08:10:05 -0700

    7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 
 """
-This script reads arguments from to a list stored in a
-JSON file named 'add_item.json', list back to the file.
+This script reads arguments from the list stored in a
+JSON file named 'add_item.json'. The uck to the file.
 
 It uses two functions:
-- load_from_json_file: Loads the list from the 'add_item.json' file.
-- save_to_json_file: Saves th list back to the 'add_item.json' file.
+- load_from_json_file: the 'add_item.json' file.
+- save_to_json_file: Saves the d_item.json' file.
 """
 
 import sys
+import os
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-items = load_from_json_file('add_item.json')
+if not os.path.exists('add_item.json'):
+    items = []
+else:
+    items = load_from_json_file('add_item.json')
 
 items.extend(sys.argv[1:])
 
