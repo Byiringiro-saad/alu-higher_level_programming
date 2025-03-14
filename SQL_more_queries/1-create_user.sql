4fdfaff2f155a98e5c440956af6d430bcfeab782
Byiringiro-saad <byiringirosaad@gmail.com>
2025-03-14 13:03:16 -0700

    another

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,5 +2,5 @@
 -- Password is set to user_0d_1_pwd and script won't fail if user already exists
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
