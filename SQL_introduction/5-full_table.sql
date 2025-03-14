c6810933fe8bcd91b44a5b46400e4b281ba599c5
Byiringiro-saad <byiringirosaad@gmail.com>
2025-03-14 11:37:03 -0700

    sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -10,5 +10,7 @@ CREATE TABLE IF NOT EXISTS first_table (
     PRIMARY KEY (id)
 );
 
--- Step 2: Display the full description of the table
-SHOW CREATE TABLE first_table\G;
+-- Step 2: Display the full description of the table in the required format
+SELECT REPLACE(SUBSTRING_INDEX(SUBSTRING_INDEX( 
+    (SELECT CREATE TABLE FROM information_schema.tables WHERE table_name = 'first_table' AND table_schema = 'hbtn_0c_0'), 
+    'CREATE TABLE ', -1), '\n', ' '), '  ', ' ');
