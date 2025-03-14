668e4db844c2784897d5776c329e1d167fa31bab
Byiringiro-saad <byiringirosaad@gmail.com>
2025-03-14 12:59:49 -0700

    another

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,17 +1,6 @@
-#!/bin/bash
-# Task: Create MySQL user 'user_0d_1' with all privileges on the MySQL server if not exists
-# This script will create the MySQL user 'user_0d_1' with all privileges on all databases and set the password to 'user_0d_1_pwd'.
-# If the user already exists, no error will occur.
+-- Script that creates the MySQL server user user_0d_1 with all privileges
+-- Password is set to user_0d_1_pwd and script won't fail if user already exists
 
-# Connect to MySQL server and run the following SQL commands
-
-mysql -u root -p <<EOF
--- Check if the user 'user_0d_1' already exists, and create it if not
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges on all databases and tables
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Flush privileges to ensure the changes take effect
 FLUSH PRIVILEGES;
-EOF
