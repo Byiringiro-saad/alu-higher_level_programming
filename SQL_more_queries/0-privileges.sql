ac4cd8bcab3d7154341c7a0041d68106a8a67584
Byiringiro-saad <byiringirosaad@gmail.com>
2025-03-23 11:38:05 -0700

    all

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,13 @@
--- Task: List all privileges of MySQL users 'user_0d_1' and 'user_0d_2' on localhost
+-- Task: Grant all privileges and list privileges of MySQL users 'user_0d_1' and 'user_0d_2' on localhost
+
+-- Grant all privileges to 'user_0d_1' on localhost
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Grant all privileges to 'user_0d_2' on localhost
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+
+-- Flush privileges to ensure changes take effect
+FLUSH PRIVILEGES;
 
 -- Show privileges for user 'user_0d_1' on localhost
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
