1ee5e9684f9b4677282955715962b8c562a605c3
Byiringiro-saad <byiringirosaad@gmail.com>
2025-03-23 11:40:45 -0700

    all

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,8 @@
--- Task: Grant all privileges and list privileges of MySQL users 'user_0d_1' and 'user_0d_2' on localhost
+-- Create user 'user_0d_1' on localhost without a password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+
+-- Create user 'user_0d_2' on localhost without a password
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 
 -- Grant all privileges to 'user_0d_1' on localhost
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
@@ -6,11 +10,11 @@ GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 -- Grant all privileges to 'user_0d_2' on localhost
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
 
--- Flush privileges to ensure changes take effect
+-- Flush privileges to apply changes
 FLUSH PRIVILEGES;
 
--- Show privileges for user 'user_0d_1' on localhost
+-- Show privileges for 'user_0d_1'
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- Show privileges for user 'user_0d_2' on localhost
+-- Show privileges for 'user_0d_2'
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
