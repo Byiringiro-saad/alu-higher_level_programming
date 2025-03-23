6e9ca4f2eb733b5885e12b54204267473d277a2c
Byiringiro-saad <byiringirosaad@gmail.com>
2025-03-23 11:42:45 -0700

    all

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,18 +1,3 @@
--- Create user 'user_0d_1' on localhost without a password
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-
--- Create user 'user_0d_2' on localhost without a password
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Grant all privileges to 'user_0d_1' on localhost
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Grant all privileges to 'user_0d_2' on localhost
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
-
--- Flush privileges to apply changes
-FLUSH PRIVILEGES;
-
 -- Show privileges for 'user_0d_1'
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
