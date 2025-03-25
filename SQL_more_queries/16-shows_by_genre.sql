bd19a5ad5a0d2a52c0628fef3b61d87715800264
Byiringiro Saad <byiringirosaad@gmail.com>
2025-03-25 15:12:52 +0200

    Update 16-shows_by_genre.sql

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,7 @@
+-- Script to list all shows and their linked genres from hbtn_0d_tvshows
+
+-- Select all tv show titles and their genre names, with NULL if no genre linked, sorted by show title and genre name
 SELECT tv_shows.title, tv_genres.name
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
-
