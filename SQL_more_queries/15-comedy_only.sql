f7382d7dd8938adc302c270d55085d4f0a0fb0b8
Byiringiro Saad <byiringirosaad@gmail.com>
2025-03-25 15:11:44 +0200

    Update 15-comedy_only.sql

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,7 @@
--- Select the titles of Comedy shows sorted in ascending order
-SELECT tv_shows.title
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN genres ON tv_show_genres.genre_id = genres.id
-WHERE genres.name = 'Comedy'
-ORDER BY tv_shows.title ASC;
+-- Script to list all Comedy shows from hbtn_0d_tvshows
 
+-- Select all tv show titles linked to the Comedy genre, sorted by show title
+SELECT tv_shows.title
+FROM tv_shows, tv_show_genres, tv_genres
+WHERE tv_genres.name = 'Comedy' AND tv_genres.id = tv_show_genres.genre_id AND tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC;
