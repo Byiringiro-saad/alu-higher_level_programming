f2832e3debc6d2dc74582aea6e41d541462e2c08
Byiringiro Saad <byiringirosaad@gmail.com>
2025-03-25 15:10:27 +0200

    Update 14-my_genres.sql

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,7 @@
--- Select genre names for the show 'Dexter' sorted in ascending order
-SELECT genres.name
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.tv_show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter'
-ORDER BY genres.name ASC;
+-- Script to list all genres of the show Dexter from hbtn_0d_tvshows
 
+-- Select all genres linked to the show Dexter, sorted by genre name
+SELECT tv_genres.name
+FROM tv_genres, tv_show_genres, tv_shows
+WHERE tv_shows.title = 'Dexter' AND tv_shows.id = tv_show_genres.show_id AND tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_genres.name ASC;
