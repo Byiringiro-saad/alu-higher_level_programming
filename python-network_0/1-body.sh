e48f7b38b10aa3c24c0295c956a65f035d802970
Byiringiro Saad <byiringirosaad@gmail.com>
2025-03-26 09:55:12 +0200

    Update 1-body.sh

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# This script sends a GET request to a URL and displays the body of the response only if the status code is 200
-curl -s -w "%{http_code}" -o response_body.txt "$1" && [ "$(tail -n 1 response_body.txt | awk '{print $1}')" -eq 200 ] && cat response_body.txt
+#!/bin/bash  
+# Sends a GET request to the URL and displays the body if status is 200  
+curl -s -o response.txt -w "%{http_code}" "$1" | grep -q "200" && cat response.txt
