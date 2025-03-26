013401d9cc3a1a888ce4ec898fa9dbd7c262bb4d
Byiringiro Saad <byiringirosaad@gmail.com>
2025-03-26 11:21:29 +0200

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,8 +6,8 @@ import urllib.request
 if __name__ == "__main__":
     url = "https://alu-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
-        body = "Custom status"
-        decoded_body = "Custom status"
+        body = response.read()
+        decoded_body = body.decode('utf-8')
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
