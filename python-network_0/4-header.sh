790589244577e8d11e1464d9cba699ef302fc1c5
Byiringiro Saad <byiringirosaad@gmail.com>
2025-03-26 10:41:40 +0200

    Update 4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script sends a GET request to a URL
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# This script sends a GET request to a URL with a custom X-HolbertonSchool-User-Id header and displays the response body
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$0"
