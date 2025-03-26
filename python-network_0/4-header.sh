7084a1a78b0fa7b173894d9c9701ee4c1cc0d271
Byiringiro Saad <byiringirosaad@gmail.com>
2025-03-26 10:11:52 +0200

    Update 4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request with a custom header and displays the response body
-curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" -H "Content-Type: application/json" "$1"
